3382684683338c2cb704ff98a73d6cb4ac73b5ac
mayank8005 <mayank8005@gmail.com>
2017-11-04 01:30:15 +0530

    minor bug fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+// if this will be false then application won't load any graph
+var allowDraw = false;
+
+
 function PieOptionTemp() {
-        this.value= 0;
-        this.label = ' ';
-        this.color = '#000'
+        this.value= ko.observable(0);
+        this.label = ko.observable('');
+        this.color = ko.observable("#000");
 }
 
 
@@ -41,8 +45,53 @@ $(document).ready(function () {
         // setting up default value 1
         this.pieNoOfOption(1);
 
+        this.pieDraw = function () {
+
+            // allowing to draw if no of option greater than 0
+            if(that.pieOptions().length > 0)
+               allowDraw = true;
+            var drawPieData = [];
+
+            that.pieOptions().forEach(function (option) {
+
+                if(!allowDraw)
+                    return;
+                drawPieData.push(koToObject(option));
+            });
+
+            // checking if error occur
+            if(!allowDraw){
+                return;
+            }
+
+            console.log(drawPieData);
+
+        };
+
     };
 
     ko.applyBindings(new ViewModel());
-    
+
+
 });
+
+function koToObject(KOFunction) {
+    var label = KOFunction.label();
+    var value= KOFunction.value();
+    var color =KOFunction.color();
+
+    // in case of error or invalid entry stopping application to draw
+    if(label===''||value<=0){
+        allowDraw = false;
+        alert('invalid label or value input');
+        return;
+    }
+
+    // returning object
+    return {
+        label: label,
+        value: value,
+        color: color
+    }
+
+}
